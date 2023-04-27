537c12e2bdd7ee534e0124abf91213167ddf228e
dinajdadollani <ddollani24@scienceleadership.org>
2023-04-27 10:30:55 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         boolean useManyThreads = true;
 
-
         FibonacciLike brr = new FibonacciLike("Sequence 1: ", 5,0,1);
 
         FibonacciLike wee = new FibonacciLike("Sequence 2: ",5, 2, 3);
