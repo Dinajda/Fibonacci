a8e3e0925015513b9e08353ca6bdb5d36b74efd5
dinajdadollani <ddollani24@scienceleadership.org>
2023-04-27 10:30:10 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        FibonacciLike brr = new FibonacciLike(5,0,1);
-        brr.run();
+        boolean useManyThreads = true;
+
+
+        FibonacciLike brr = new FibonacciLike("Sequence 1: ", 5,0,1);
+
+        FibonacciLike wee = new FibonacciLike("Sequence 2: ",5, 2, 3);
+
+        if(useManyThreads) {
+            Thread numLaunchingThread = new Thread(brr);
+            Thread num2LaunchingThread = new Thread(wee);
+            numLaunchingThread.start();
+            num2LaunchingThread.start();
+
+        }
+        else {
+            brr.run();
+            wee.run();
+        }
     }
 }
